feae9cc76ace04177f1cb238ca2ee1c57bc5d286
Governess <governesss515@gmail.com>
2023-11-01 18:03:36 -0700

    display array values on-screen after onclick

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 // Initial set of colors to cycle through based on user selection.
-const colorCarousel = {
+let colorCarousel = {
     'red': ['garnet', 'currant', 'sangria'],
     'blue': ['azure', 'lapis', 'admiral'],
     'green': ['chartreuse', 'parakeet', 'fern'],
     'yellow': ['canary', 'tuscan_sun', 'daffodil']
 }
 
+// colorCarousel = JSON.stringify(colorCarousel)
+
+function setUniqueColor(id, color){
+    return id.innerHTML = color;
+}
+
 function changeColor(selectedColor){
     // Injects the stringified array into the HTML based on the initial ID.
-    const carouselID = document.getElementById("carousel");
-    carouselID.innerHTML = JSON.stringify(colorCarousel[selectedColor]);
+    // const carouselID = document.getElementById("carousel");
+    const color_1 = document.getElementById("color_1");
+    const color_2 = document.getElementById("color_2");
+    const color_3 = document.getElementById("color_3");
+
+    const selectedColorArray = colorCarousel[selectedColor];
+    console.log(selectedColorArray)
+
+    // For each unique color (inside the bucket of the original color category), we will display them on-screen.
+    selectedColorArray.forEach(color => {
+        console.log(color, typeof color)
+        console.log(selectedColorArray[0], selectedColorArray[1], selectedColorArray[2])
+        switch(true){
+            case selectedColorArray[0] === color:
+                setUniqueColor(color_1, color);
+                break;
+            case selectedColorArray[1] === color:
+                setUniqueColor(color_2, color);
+                break;
+            case selectedColorArray[2] === color:
+                setUniqueColor(color_3, color);
+                break;
+            default:
+                console.log(`Error. ${color} does not equal ${selectedColorArray[0]}`);
+                break;
+        }
+    });
 }
